c2f8c7e754c8ebfaabd6df74dd1597476d84711e
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 17:04:08 +0530

    Create principal.py

---
+++ principal.py
@@ -0,0 +1,13 @@
+import sys
+if len len(sys.argv) !=4:
+   print("Usage: python simple_interest.py <principal> <rate> <time>")
+     sys.exit()
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
+
+simple_interest =(principal * rate * time)/100
+print("Principal Amount:",principal)
+print("Rate of Interest:",rate)
+print("Time period:",time)
+print("Simple Interest:",simple_inerest)
