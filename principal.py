35ca7e8ec09611ca65098e444b12d3cf4d4ce8aa
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 17:36:10 +0530

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,10 +1,9 @@
 import sys
-if len len(sys.argv)==4:
   script_name=sys.arvg[0]
 principal = float(sys.argv[1])
 rate = float(sys.argv[2])
 time = float(sys.argv[3])
-else:
+
   simple_interest =(principal * rate * time)/100
   print("Principal Amount:",principal)
   print("Rate of Interest:",rate)
