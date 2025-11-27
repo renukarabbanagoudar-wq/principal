bc290ea862fec99f42303b34c36c2cef8c62d4e0
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 17:27:11 +0530

    Update principal.py

--- principal.py
+++ principal.py
@@ -4,9 +4,9 @@ if len len(sys.argv)==4:
 principal = float(sys.argv[1])
 rate = float(sys.argv[2])
 time = float(sys.argv[3])
-
-simple_interest =(principal * rate * time)/100
-print("Principal Amount:",principal)
-print("Rate of Interest:",rate)
-print("Time period:",time)
-print("Simple Interest:",simple_inerest)
+else:
+  simple_interest =(principal * rate * time)/100
+  print("Principal Amount:",principal)
+  print("Rate of Interest:",rate)
+  print("Time period:",time)
+  print("Simple Interest:",simple_inerest)
